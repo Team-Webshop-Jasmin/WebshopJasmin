6c16013c023b08ac9cc1f979ede99e26840f1403
WilliamxLau <williamlau158@gmail.com>
2022-04-19 20:56:48 +0200

    Revert Changes

--- Website/jasminsweets_ws/content/login/index.js
+++ Website/jasminsweets_ws/content/login/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
 function initNetlifyIdentity() {
   console.log("initNetlifyIdentity called.")
   const script = document.createElement("script");
@@ -31,14 +35,14 @@ class NetlifyIdentity extends Component {
 }
 
 const IndexPage = () => {
-  initNetlifyIdentity();
-
   return(
       <Layout>
         <NetlifyIdentity />
         <SEO title="Home" />
         <h1>Hi people</h1>
         <h2 onClick={() => { openNetlifyModal() }}>Login</h2>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <Image />
         </div>
