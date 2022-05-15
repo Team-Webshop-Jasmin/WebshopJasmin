229ec6ce59333b81cf1e7b00785fe4a6ae5f829f
WilliamxLau <williamlau158@gmail.com>
2022-05-16 00:44:23 +0200

    concept of admin

--- Website/Backend/Admin seite Markdown editor/public/main.js
+++ Website/Backend/Admin seite Markdown editor/public/main.js
@@ -5,8 +5,6 @@ const converter = new showdown.Converter();
 textEditor.addEventListener("keyup", evt => {
   const { value } = evt.target;
 
-  window.localStorage.setItem('markdown', value); 
-
   const html = converter.makeHtml(value);
 
   preview.innerHTML = html;
