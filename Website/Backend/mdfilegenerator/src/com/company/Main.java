8942ef328a87fb2e2833f03fdd9802e4254c3984
WilliamxLau <williamlau158@gmail.com>
2022-05-28 18:55:40 +0200

    work on the mdfile generator added

--- Website/Backend/mdfilegenerator/src/com/company/Main.java
+++ Website/Backend/mdfilegenerator/src/com/company/Main.java
@@ -1,24 +1,43 @@
 package com.company;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.desktop.ScreenSleepEvent;
+import java.io.*;
 import java.util.Scanner;
 import static java.sql.DriverManager.println;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String fs ="";
+        String fileContent = fileReader(fs);
+        String newfileContent = SearchAndReplace(fileContent);
+
+        System.out.println(newfileContent);
+        fileWriter(newfileContent);
+    }
+
+    public static String fileReader(String fc) throws FileNotFoundException {
         File file = new File("mdtemplate.md");
-        String fileContent = "";
         Scanner scan = new Scanner(file);
 
         while (scan.hasNextLine()) {
-            fileContent = fileContent.concat(scan.nextLine() + "\n");
+            fc = fc.concat(scan.nextLine() + "\n");
         }
 
-        FileWriter writer = new FileWriter("newproduct.md");
+        return fc;
+    }
+    public static Writer fileWriter(String fileContent) throws IOException {
+        FileWriter writer = new FileWriter("C:\\Users\\willi\\Desktop\\Schule\\SYP\\Project Repo\\Website\\jasminsweets_ws\\content\\products\\newproduct.md");
+
         writer.write(fileContent);
         writer.close();
+        return writer;
+    }
+
+    public static String SearchAndReplace (String fc){
+        String name = "Schweinefleisch";
+        String price = "22,50";
+        fc = fc.replace("%%TITLE%%", name);
+        fc = fc.replace("%%PRICE%%", price);
+        return fc;
     }
 }
--- Website/jasminsweets_ws/content/products/newproduct.md
+++ Website/jasminsweets_ws/content/products/newproduct.md
@@ -1,5 +1,5 @@
 ---
-title: "%%TITLE%%"
+title: "Schweinefleisch"
 date: 2019-10-17T11:22:16+06:00
 draft: false
 
@@ -7,7 +7,7 @@ draft: false
 description : ""
 
 # product Price
-price: "%%PRICE%%"
+price: "22,50"
 beforeprice: ""
 
 # Product Short Description
