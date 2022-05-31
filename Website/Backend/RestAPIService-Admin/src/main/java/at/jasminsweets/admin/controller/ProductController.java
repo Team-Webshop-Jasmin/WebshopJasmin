602850f9610d361ae9e139437df5ca0950837f81
Toni2735 <62690585+Toni2735@users.noreply.github.com>
2022-05-31 20:31:25 +0200

    Update

---
+++ Website/Backend/RestAPIService-Admin/src/main/java/at/jasminsweets/admin/controller/ProductController.java
@@ -0,0 +1,11 @@
+package at.jasminsweets.admin.controller;
+
+import javax.ws.rs.*;
+
+import javax.ws.rs.core.MediaType;
+
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductController {
+}
