92282ec1392f49e5ee189d2ae1820eb53dfa5268
Toni2735 <62690585+Toni2735@users.noreply.github.com>
2022-05-31 19:50:36 +0200

    Update

---
+++ Website/Backend/RestAPIService-Admin/src/main/java/at/jasminsweets/admin/persistence/entities/Product.java
@@ -0,0 +1,67 @@
+package at.jasminsweets.admin.persistence.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = true)
+    private String shortDescription;
+
+    @Column(nullable = true)
+    private String longDescription;
+
+    @OneToOne
+    @JoinColumn(name ="snipCart_Id")
+    private SnipCart snipCart;
+
+    @ManyToOne
+    @JoinColumn(name ="category_id")
+    private Category category;
+
+    public Product(){}
+
+    public Product(String title, double price, String shortDescription, String longDescription) {
+        this.title = title;
+        this.price = price;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public SnipCart getSnipCart() {
+        return snipCart;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+}
