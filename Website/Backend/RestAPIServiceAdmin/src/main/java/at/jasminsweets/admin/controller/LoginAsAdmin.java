81deb3351e1797c4f0e62e05aa7d68e6f4da3d0a
YimneRaid <43366463+YimneRaid@users.noreply.github.com>
2022-06-12 19:08:34 +0200

    login

--- Website/Backend/RestAPIServiceAdmin/src/main/java/at/jasminsweets/admin/controller/LoginAsAdmin.java
+++ Website/Backend/RestAPIServiceAdmin/src/main/java/at/jasminsweets/admin/controller/LoginAsAdmin.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Path("/admin")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginAsAdmin {
     private boolean islogged = false;
