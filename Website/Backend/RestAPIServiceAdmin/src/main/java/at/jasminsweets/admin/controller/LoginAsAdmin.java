2b50af449da0d590682d71da458d290a03ab8e59
YimneRaid <43366463+YimneRaid@users.noreply.github.com>
2022-06-12 18:48:07 +0200

    Quarkus doesnt start

--- Website/Backend/RestAPIServiceAdmin/src/main/java/at/jasminsweets/admin/controller/LoginAsAdmin.java
+++ Website/Backend/RestAPIServiceAdmin/src/main/java/at/jasminsweets/admin/controller/LoginAsAdmin.java
@@ -11,7 +11,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/admin")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginAsAdmin {
     private boolean islogged = false;
